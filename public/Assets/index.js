504948b8ad64036f2e7914beaa3edaede0e11527
romeoscript <romeobourne211@gmail.com>
2024-05-23 23:04:56 +0100

    web p

--- public/Assets/index.js
+++ public/Assets/index.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import people from './people.svg';
-import humanPhoto from './human-photo .jpeg'
-import amazon from './amazon.jpeg';
-import wallmart from './wallmart.jpeg';
+import humanPhoto from './human-photo .webp'
+import amazon from './amazon.webp';
+import wallmart from './wallmart.webp';
 import google from './google.jpeg';
 
 
