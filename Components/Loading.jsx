fb1daff95916c6ce72780764e45e84d4d15e1c79
romeoscript <romeobourne211@gmail.com>
2024-05-23 21:12:29 +0100

    reduced it

---
+++ Components/Loading.jsx
@@ -0,0 +1,11 @@
+// components/Loading.js
+export default function Loading() {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+  
--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ const People = lazy(() => import("@/Components/People"));
 const Result = lazy(() => import("@/Components/Result"));
 const ParallaxCard = lazy(() => import("@/Components/parrallax"));
 const Service = lazy(() => import("@/Components/Service"));
+const Loading = lazy(() => import("@/Components/Loading"));
 
 // Import assets
 import { amazon, wallmart, google } from "@/public/Assets";
@@ -21,33 +22,25 @@ export default function Home() {
         duration: 800,
         once: false,
       });
-      Aos.initialized = true;
+      Aos.initialized = true; // Ensure Aos is only initialized once
     }
   }, []);
 
   return (
-    <main>
-      <Suspense>
+    <Suspense fallback={<Loading />}>
+      <main>
         <Hero />
-      </Suspense>
-      <Suspense>
         <Partners />
-      </Suspense>
-      <Suspense>
         <Service />
-      </Suspense>
-      <Suspense>
         <Result />
-      </Suspense>
-      <section className="relative bg-white z-10">
-        <h2 className="text-3xl font-bold p-[1rem] text-center text-gray-800 mb-4">
-          Case Studies
-        </h2>
-        <p className="text-center mb-[3rem] w-4/5 m-auto">
-          Here are the best features that makes margin the most powerful, fast and user-friendly platform.
-        </p>
-        <div className="grid md:grid-cols-3 gap-4 md:px-[2rem] px-[1rem] pb-[3rem]">
-          <Suspense>
+        <section className="relative bg-white z-10">
+          <h2 className="text-3xl font-bold p-[1rem] text-center text-gray-800 mb-4">
+            Case Studies
+          </h2>
+          <p className="text-center mb-[3rem] w-4/5 m-auto">
+            Here are the best features that makes margin the most powerful, fast and user-friendly platform.
+          </p>
+          <div className="grid md:grid-cols-3 gap-4 md:px-[2rem] px-[1rem] pb-[3rem]">
             <ParallaxCard
               Image={amazon.src}
               title="AMAZON Inc."
@@ -58,8 +51,6 @@ export default function Home() {
               optimization, content marketing strategy development, and email campaign design. The result? A 70% increase in website
               traffic and a 40% increase in sales within just 6 months!"
             />
-          </Suspense>
-          <Suspense>
             <ParallaxCard
               Image={wallmart.src}
               title="WALMART CORPORATION"
@@ -67,20 +58,16 @@ export default function Home() {
               comprehensive SEO and email marketing strategy that included targeted keyword research, technical SEO, and email
               campaign design. The result? A 30% increase in website traffic and a 25% increase in conversions within just 3 months!"
             />
-          </Suspense>
-          <Suspense>
             <ParallaxCard
               Image={google.src}
               title="GOOGLE LLC"
               text="Google LLC was looking to increase their market share and needed a way to attract new customers. They implemented a 
               strategic development plan that included a unique value proposition, a target audience analysis, and a comprehensive marketing"
             />
-          </Suspense>
-        </div>
-      </section>
-      <Suspense>
+          </div>
+        </section>
         <People />
-      </Suspense>
-    </main>
+      </main>
+    </Suspense>
   );
 }
