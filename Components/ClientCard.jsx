cea15bc049d1def19c7058e8ff665135b1866733
romeoscript <romeobourne211@gmail.com>
2024-05-23 23:29:26 +0100

    done

--- Components/ClientCard.jsx
+++ Components/ClientCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ClientCard = ({ name, testimonial, image }) => {
     return (
         <div className='border-[1px] m-[1rem] border-[#176FF5] shadow-md text-sm rounded-md flex items-center flex-col justify-center text-center gap-4 p-[2rem]'>
             <p>{testimonial}</p>
-            <img 
-                className='w-[45px] h-[45px] rounded-full' 
-                src={image} 
-                alt={`${name}'s photo`} 
+            <Image
+                className='rounded-full'
+                src={image}
+                alt={`${name}'s photo`}
+                width={45}
+                height={45}
+                objectFit="cover"
             />
             <h2>{name}</h2>
         </div>
