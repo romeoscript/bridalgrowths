b81a90e94877496d3af0fe2d47d788defda446f2
romeoscript <romeobourne211@gmail.com>
2024-05-23 22:44:14 +0100

    loading image

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className='relative w-5/5  mt-[75px] ' style={{ height: 'calc(80vh - 80px)' }}>
             <video autoPlay loop muted className='absolute top-0 left-0 h-full w-full object-cover'>
-                <source src="/Assets/bg-video.mp4" type="video/mp4" className='w-full h-full' />
+                <source src="/Assets/bg-video.webm" type="video/mp4" className='w-full h-full' />
                 Your browser does not support the video tag.
             </video>
             <div className='font-crimson absolute top-0 left-0 w-full h-full bg-[#010814cc] flex flex-col justify-center items-center text-white'>
