05dec5416f42eafe49bd891d7e461835992f69e8
romeoscript <romeobourne211@gmail.com>
2024-05-24 04:05:50 +0100

    done

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
     return (
-        <div className='relative w-5/5  mt-[75px] ' style={{ height: 'calc(80vh - 80px)' }}>
+        <div className='relative w-5/5 mt-[75px]' style={{ height: 'calc(80vh - 80px)' }}>
             <video autoPlay loop muted className='absolute top-0 left-0 h-full w-full object-cover'>
-                <source src="/Assets/bg-video.webm" type="video/mp4" className='w-full h-full' />
+                <source src="/Assets/bg-video.mp4" type="video/mp4" />
+                <source src="/Assets/bg-video.webm" type="video/webm" />
                 Your browser does not support the video tag.
             </video>
             <div className='font-crimson absolute top-0 left-0 w-full h-full bg-[#010814cc] flex flex-col justify-center items-center text-white'>
@@ -13,11 +14,10 @@ const Hero = () => {
                     <p className='md:text-2xl w-full m-auto p-[1rem] my-[1rem]'>Are you tired of feeling like your business is stuck in neutral? Are you struggling to attract new customers and increase
                         revenue? Well, I’ve got good news for you! With the right combination of SEO and email marketing strategies, you can unlock
                         the full potential of your business and watch your revenue soar!</p>
-
                 </div>
             </div>
         </div>
     )
 }
 
-export default Hero
+export default Hero;
